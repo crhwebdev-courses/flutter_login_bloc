a0a9285e5c6cb68535bfa9ab8bc1d9b39117eb53
Carl Hain <crhain@gmail.com>
2019-03-22 12:17:44 -0500

    Add email and password fields to LoginScreen widget

--- lib/src/screens/login_screen.dart
+++ lib/src/screens/login_screen.dart
@@ -5,7 +5,30 @@ class LoginScreen extends StatelessWidget {
     return Container(
       margin: EdgeInsets.all(20.0),
       child: Column(
-        children: [],
+        children: [
+          emailField(),
+          passwordField(),
+        ],
+      ),
+    );
+  }
+
+  Widget emailField() {
+    return TextField(
+      keyboardType: TextInputType.emailAddress,
+      decoration: InputDecoration(
+        hintText: 'you@example.com',
+        labelText: 'Email Address',
+      ),
+    );
+  }
+
+  Widget passwordField() {
+    return TextField(
+      obscureText: true,
+      decoration: InputDecoration(
+        hintText: 'password',
+        labelText: 'Password',
       ),
     );
   }
