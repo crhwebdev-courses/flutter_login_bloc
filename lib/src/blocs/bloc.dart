be4478b342f8716b8d85bd98b723df01d994dddd
Carl Hain <crhain@gmail.com>
2019-03-23 09:42:56 -0500

    Add getters for email and password stream functions to Bloc class

--- lib/src/blocs/bloc.dart
+++ lib/src/blocs/bloc.dart
@@ -1,3 +1,14 @@
 import 'dart:async';
 
-class Bloc {}
+class Bloc {
+  final _emailController = StreamController<String>();
+  final _passwordController = StreamController<String>();
+
+  // Getters for Adding data to stream
+  Function(String) get changeEmail => _emailController.sink.add;
+  Function(String) get changePassword => _passwordController.sink.add;
+
+  //Getters for Retrieving data from stream
+  Stream<String> get email => _emailController.stream;
+  Stream<String> get password => _passwordController.stream;
+}
