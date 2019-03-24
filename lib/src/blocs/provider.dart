dcb94e2d490cfba08a2650b98c766e08b4e2e786
Carl Hain <crhain@gmail.com>
2019-03-24 09:49:44 -0500

    Add Provider class that will be used by widgets to access scoped instances of Bloc class

---
+++ lib/src/blocs/provider.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'bloc.dart';
+
+class Provider extends InheritedWidget {
+  final Bloc bloc = Bloc();
+
+  bool updateShouldNotify(_) => true;
+
+  //context object carries the information about the widget and all enclosing widgets
+  static Bloc of(BuildContext context) {
+    //look up context tree untill reaching Provider widget, return it type cast as a Provider widget, and
+    // then get the bloc object from that
+    return (context.inheritFromWidgetOfExactType(Provider) as Provider).bloc;
+  }
+}
